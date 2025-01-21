5796a141cc85c25dcdb267f9a77ad236d4c34fd6
eduardojacinto <ejacinto2198@gmail.com>
2025-01-21 15:07:01 -0500

    spacing

--- FileParser.py
+++ FileParser.py
@@ -11,6 +11,7 @@ def ParsePokedex(pokedex):
 
     # do the block of code while opening the file and setting a file pointer named csvfile
     with open("pokemon-data.csv", "r") as csvfile:
+        
         # create a parser that will read in each line and separate by comma
         parser = csv.reader(csvfile, delimiter=',')
 
@@ -18,8 +19,10 @@ def ParsePokedex(pokedex):
         next(parser)
         # loop through each line in the file
         for line in parser:
-            print("This is the pokemon data:")
-            print(line)
+
+            # print("This is the pokemon data:")
+            # print(line)
+
             # need to parse the list of moves out of the 7th index
             # use ast.literal_eval() to get the list out of the csv
             moves = ''
@@ -51,6 +54,7 @@ def ParseMoves(move_set):
 
         # eliminate the header of the file
         next(parser)
+
         # loop through each line in the file
         for line in parser:
             
@@ -59,7 +63,7 @@ def ParseMoves(move_set):
 
             # this handles type error if accuracy in csv is None
             accuracy = None if line[6].strip() == 'None' else int(line[6])
-            
+
             # create move objects
             curr_move = p.Move(line[0], line[1], line[2], line[3], int(line[4]), int(line[5]), accuracy)
             # add move object to the list of existing moves
